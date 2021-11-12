c47066b63dc4f54f6ecdfd3e499489ba55e5f771
Nicolas Ruiz <syngenruiz2@gmail.com>
2021-11-12 05:34:26 -0600

    tried adding login

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const mongoose = require('mongoose')
+const { findOne } = require('../models/user')
 const db = "mongodb+srv://MrSyn:2HutKNWSgJO4g3SX@nicocluster.fjnup.mongodb.net/eventsdb?retryWrites=true&w=majority"
 
 mongoose.connect(db, err => {
@@ -24,6 +25,24 @@ router.post('/register', (req, res) => {
             console.log(error)
         } else {
             res.status(200).send(registeredUser)
+        }
+    })
+})
+
+router.post('./login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({ email: userData.email }, (error, user) => {
+        if (error) {
+            console.log(error)
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid email')
+            } else if (user.password !== userData.password) {
+                res.status(401).send('Invalid Password')
+            } else {
+                res.status(200).send(user)
+            }
         }
     })
 })
